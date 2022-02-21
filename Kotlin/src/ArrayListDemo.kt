02e97901a80321574a3e4658ebda142250cc786d
ophthalmologist <gusrylmubarok@gmail.com>
2022-02-21 12:24:32 +0700

    Collections

---
+++ Kotlin/src/ArrayListDemo.kt
@@ -0,0 +1,12 @@
+fun main(args:Array<String>) {
+    var arrayList = ArrayList<String>()
+    arrayList.add("Apple")
+    arrayList.add("Pinnaple")
+    arrayList.add("Orange")
+    arrayList.add("Grave")
+
+    print("Fruits: ")
+    for(item in arrayList) {
+        print(item)
+    }
+}
---
+++ Kotlin/src/String.kt
@@ -0,0 +1,9 @@
+fun main (args:Array<String>) {
+    var title = "Welcome to New Yourk"
+    println(title)
+    println("Title : " + title)
+    println("Title $title")
+    var name = "Gusryl" + "Mubarok"
+    print("Full Name: $name")
+    
+}
