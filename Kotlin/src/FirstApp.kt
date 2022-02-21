bad333d67a99996a285b92c132e74411313708f4
ophthalmologist <gusrylmubarok@gmail.com>
2022-02-21 11:06:48 +0700

    Variabel

---
+++ Kotlin/src/FirstApp.kt
@@ -0,0 +1,5 @@
+fun main(args:Array<String>) {
+    print("Hi Gusryl Mubarok,\n")
+    print("Welcome to Kotlin")
+    print(3)
+}
---
+++ Kotlin/src/NullSafety.kt
@@ -0,0 +1,7 @@
+fun main(args:Array<String>) {
+    // This is nullsafety if use ? //question mark
+    var name:String?
+    name = null
+    print(name)
+
+}
