02da0d4400f7b302e1da4cf967e56412531224fa
Agus S. Mubarok <gusrylmubarok@gmail.com>
2023-07-12 19:02:50 +0700

    setup project

---
+++ src/main/kotlin/org/example/kotlin/EgKotlinSpringWebfluxApplication.kt
@@ -0,0 +1,11 @@
+package org.example.kotlin
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class EgKotlinSpringWebfluxApplication
+
+fun main(args: Array<String>) {
+	runApplication<EgKotlinSpringWebfluxApplication>(*args)
+}
